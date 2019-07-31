660ee6ca7ac56bf621fe07e36ebf89612dbf39c9
yangyang <1208488082@qq.com>
2019-07-31 21:55:12 +0800

    p2p项目开启

---
+++ src/test/java/com/hy/moneyplanning/managemoney/ManagemoneyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hy.moneyplanning.managemoney;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagemoneyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
