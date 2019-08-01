7ea3b39efac51eaa2a0890524651a9fd8ba922eb
yangyang <1208488082@qq.com>
2019-08-01 19:16:31 +0800

    p2p项目主页项目展示

--- src/main/java/com/hy/moneyplanning/controller/ProjectController.java
+++ src/main/java/com/hy/moneyplanning/controller/ProjectController.java
@@ -10,30 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-//@RequestMapping("/project")
+@RestController
+@RequestMapping("/project")
 public class ProjectController {
 
     @Autowired
     ProjectService projectService;
 
-    @RequestMapping("/listss")
-    @ResponseBody
+    /*
+    * 查询项目列表
+    * */
+    @RequestMapping("/list")
     public List<Project> showList(){
 
         List<Project> projectList = projectService.showList();
 
-        System.out.println("666666");
+        //System.out.println("666666");
         return projectList;
     }
 
-    @RequestMapping("/demo")
-    @ResponseBody
-    public List<Project> demo(){
-
-        List<Project> projectList = projectService.showList();
-
-        System.out.println("666666");
-        return projectList;
-    }
 }
