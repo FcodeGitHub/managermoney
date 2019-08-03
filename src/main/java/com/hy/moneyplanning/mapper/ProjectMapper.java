302909653ac1c0194a547a2cea8c41b206524b96
dsq <739437747@qq.com>
2019-08-03 16:47:40 +0800

    dsq08

--- src/main/java/com/hy/moneyplanning/mapper/ProjectMapper.java
+++ src/main/java/com/hy/moneyplanning/mapper/ProjectMapper.java
@@ -20,4 +20,24 @@ public interface ProjectMapper {
      * 通过id查询
      */
     Project selectProjectDetailById(int id);
+
+    /**
+     * 通过项目id查询项目描述
+     */
+    Company selectProjectById(int id);
+
+    /**
+     * 通过项目id查询风控信息
+     */
+    ControlInfo selectControlInfo(int id);
+
+    /**
+     * 通过id查询保障措施
+     */
+    Safety selectSafety(Integer safetyId);
+
+    /**
+     * 通过id查询风险控制措施
+     */
+    RiskControl selectRiskControl(Integer riskcontrolId);
 }
